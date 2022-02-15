6207ab625f119cd50ba0683ab409c5a30d926d42
Tiago Silvestre <tiagoa3203@gmail.com>
2022-02-15 23:03:01 +0000

    Added background class (not done yet)

---
+++ src/background.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "entity.h"
+#include "animation.h"
+#include "textureManager.h"
+
+class background : public entity
+{
+    public:
+        background(textureManager* manager , timer* t)
+        {
+            animationInfo info;
+            info.trajectory.push_back(p2d<float>(0,0));
+            info.trajectory.push_back(p2d<float>(1000 , 0));
+            info.repeat = true;
+            info.type = animationType::linear;
+            info.duration = 5000.0;
+
+            texSet->add_texture( , 0);
+            if(!manager->contains(TEXTURE::BACK1))
+            {
+                manager->add_texture(TEXTURE::BACK1 , )
+            }
+
+            anim = new animation(this , t , info);
+        }
+
+        ~background()
+        {
+
+        }
+    private:
+        textureManager* t_manager = nullptr;
+        animation* anim = nullptr;
+        
+};
+
