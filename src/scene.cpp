4cdcf9b3ba8990eb4bfa46000977f7cf4d441426
Tiago Silvestre <tiagoa3203@gmail.com>
2022-02-23 23:00:28 +0000

    created scene_objects struct / vars variable

--- src/scene.cpp
+++ src/scene.cpp
@@ -2,59 +2,94 @@
 
 scene::~scene()
 {
-    delete clock;
+    delete scene::vars;
 }
 
 void scene::show_screen_info()
 {
-    double fps = clock->get_fps();
-    const SDL_FRect* dim = _camera->get_dimension();
+    double fps = scene::vars->clock->get_fps();
+    const SDL_FRect* dim = scene::vars->view->get_dimension();
     std::string str = "FPS:" + std::to_string(fps) + "| x:" + std::to_string((int)dim->x) + ",y:" +
     std::to_string((int)dim->y) + ",w:"+std::to_string((int)dim->w)+",h:"+std::to_string((int)dim->h);
     
-    SDL_SetWindowTitle(window , str.c_str());
+    SDL_SetWindowTitle(scene::vars->window , str.c_str());
 
 }
 
 scene_id scene::process(SDL_Renderer* _renderer , SDL_Window* _window)
 {
-    this->window = _window;
-    this->renderer = _renderer;
-
-    clock = new timer(60.0 , 60.0);
-    texture_manager = new textureManager(_renderer);
-
-    _camera = new camera(_renderer , texture_manager , _window);
+    if(_renderer == nullptr || _window == nullptr)
+    {
+        throw std::runtime_error("RENDERER OR WINDOW == NULLPTR");
+        return scene_id::end;
+    }
+    
+    scene::vars = new scene_objects();
+    vars->initialize_variables(_renderer , _window);
     
     std::function<void()> f = std::bind(&scene::show_screen_info , this);
-    clock->newTimeEvent(EVENTS::SHOW_SCREEN_FPS , 1000 , f);
+    scene::vars->clock->newTimeEvent(EVENTS::SHOW_SCREEN_FPS , 1000 , f);
 
     init();
-
     while(isRunning)
     {
-       clock->start_frame();
+       scene::vars->clock->start_frame();
         
         handleEvents();
 
-        if(clock->canTickCps())
+        if(scene::vars->clock->canTickCps())
         {
 
-            clock->cpsTick();
+            scene::vars->clock->cpsTick();
             updateLogic();
-            clock->process_time_events();
-            clock->update_dt_functions();
+            scene::vars->clock->process_time_events();
+            scene::vars->clock->update_dt_functions();
         }
-        if(clock->canTickFps())
+        if(scene::vars->clock->canTickFps())
         {
 
-            clock->fpsTick();
+            scene::vars->clock->fpsTick();
             updateGraphics();
         }
 
-        clock->end_frame();
-        clock->wait();
+        scene::vars->clock->end_frame();
+        scene::vars->clock->wait();
     }
 
     return quit();
 }
+
+scene_objects::scene_objects()
+{
+
+}
+
+void scene_objects::initialize_variables(SDL_Renderer* r , SDL_Window* w)
+{
+    renderer = r;
+    window   = w;
+    clock = new timer(60.0 , 60.0);
+
+    texture_manager = new textureManager();
+
+    hitbox_manager = new hitboxManager();
+
+    entity_manager = new entityManager();
+
+    chunk_manager = new chunkManager();
+
+    view = new camera();
+
+}
+
+scene_objects::~scene_objects()
+{
+    delete texture_manager;
+    delete hitbox_manager;
+    delete entity_manager;
+    delete chunk_manager;
+    delete clock;
+    delete view;
+}
+
+scene_objects* scene::vars = nullptr;
--- src/scene.h
+++ src/scene.h
@@ -1,11 +1,17 @@
 #pragma once
 
-#include "timer.h"
-#include <iostream>
-#include "SDL2/SDL.h"
-#include "camera.h"
-#include "textureManager.h"
+
 #include <string.h>
+#include <iostream>
+#include <SDL2/SDL.h>
+
+#include "textureManager.h"
+#include "hitboxManager.h"
+#include "entityManager.h"
+#include "chunkManager.h"
+#include "timer.h"
+#include "camera.h"
+
 
 enum scene_id
 {
@@ -13,13 +19,44 @@ enum scene_id
 };
 
 
+
+
+class textureManager;
+class hitboxManager ;
+class entityManager ;
+class chunkManager  ;
+class timer         ;
+class camera        ;
+
+struct scene_objects
+{
+    scene_objects();
+    ~scene_objects();
+    void initialize_variables(SDL_Renderer* r , SDL_Window* w);
+
+
+    textureManager*        texture_manager;
+    hitboxManager *        hitbox_manager;
+    entityManager *        entity_manager;
+    chunkManager  *        chunk_manager;
+    timer         *        clock;
+    camera        *        view;
+    SDL_Renderer  *        renderer;
+    SDL_Window    *        window;
+};
+
+
+
+
 /*
 scene class implements a generic game loop.
-It contains all objects necessary (or unecessary) to run the game.
+It contains all objects necessary  to run the game.
 
 ()
 
 */
+
+
 
 
 class scene
@@ -34,34 +71,17 @@ class scene
          virtual void updateGraphics() = 0;
          virtual scene_id quit() = 0;
 
+        static scene_objects* vars;
         
     protected:
 
         void show_screen_info();
 
     scene_id next_scene;
-    timer* clock = nullptr;
     bool isRunning = true;
-
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
-
     SDL_Event event;
-
-    camera* _camera = nullptr;
-
-
-    //MANAGERS VARIABLES
-    textureManager* texture_manager = nullptr;
-
     
 
     private:
-    
-
-
-
-    
-
 };
 
