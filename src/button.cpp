b2b820fe2ac2898f0166d3860c55e158d223d2fa
Tiago Silvestre <tiagoa3203@gmail.com>
2022-02-23 23:04:35 +0000

    added scene vars variable support,  bug fixes

--- src/button.cpp
+++ src/button.cpp
@@ -1,32 +1,39 @@
 #include "button.h"
 
-button::button(std::string text , textureManager* _tManager , hitboxManager* hitbox_manager , std::function<void()> onclick)
+
+void button::on_id_change(int lastid , int newid)
+{
+    texSet->set.at(1).id = newid;
+    scene::vars->texture_manager->transfer(lastid , newid);
+    scene::vars->hitbox_manager->transfer(lastid , newid);
+}
+
+button::button(std::string text , std::function<void()> onclick)
 {
     entity::world_coordinate = false;
     this->text = text;
-    this->t_manager = _tManager;
-    this->hitbox_manager = hitbox_manager;
 
-    t_manager->add_text(entity::id , text.c_str());
-    
+    scene::vars->texture_manager->add_text(entity::id , text.c_str());
+
     textureInfo info1(this->get_size());
     textureInfo info2(this->get_size());
     info1.id = entity::id;
     info1.size_unit = UNIT::PX;
-    info1.size = t_manager->get_text_size(text);
+    info1.size = scene::vars->texture_manager->get_text_size(text);
     info1.position_unit = UNIT::CENTERED;
 
     info2.id = (int)TEXTURES::BUTTON;
 
 
-    texSet->add_texture(info1,       1);
     texSet->add_texture(info2      , 0);
-    hitbox_manager->new_hitbox(entity::id , this , onclick);
+    texSet->add_texture(info1,       1);
+
+    scene::vars->hitbox_manager->new_hitbox(entity::id ,onclick);
 }
 
 button::~button()
 {
     //deassociate from hitbox_manager
-    hitbox_manager->remove(entity::id);
-    t_manager->remove(entity::id);
+    scene::vars->hitbox_manager->remove(entity::id);
+    scene::vars->texture_manager->remove(entity::id);
 }
--- src/button.h
+++ src/button.h
@@ -1,8 +1,10 @@
+#pragma once
+
+
 #include "entity.h"
 #include <string>
 #include "animation.h"
-#include "hitboxManager.h"
-#include "textureManager.h"
+#include "scene.h"
 /*
     button is a rectangle with a text inside
 
@@ -10,12 +12,12 @@
 class button : public entity
 {
     public:
-        button(std::string text , textureManager* _tManager , hitboxManager* hitbox_manager , std::function<void()> onclick);
+        button(std::string text , std::function<void()> onclick);
         ~button();
     private:
     std::string text;
-    textureManager* t_manager = nullptr;
-    hitboxManager* hitbox_manager = nullptr;
+
+    void on_id_change(int lastid , int newid) override;
 
 
 };
